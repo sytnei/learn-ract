cf488b1b660c000e9afc5413f91429d606e61bb5
sytnei <ionut_dina@yahoo.com>
2022-11-06 11:15:34 +0200

    Cleanup code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useParams } from "react-router-dom"; 
 import Card from 'react-bootstrap/Card';
 import getCourses from '../../services/getCourses';
 
