ab4ab05459c69d000c8a82272de49cd67078a441
linvid <54632332+linvid@users.noreply.github.com>
2022-11-08 20:18:50 +0200

    breadcrumbs

--- src/components/pages/Course.js
+++ src/components/pages/Course.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from "react-router-dom";
 import getPosts from '../../services/getPosts';
-
+import ListGroup from 'react-bootstrap/ListGroup';
 import { useState, useEffect } from 'react';
+import { Breadcrumb } from 'react-bootstrap';
 
 function Course() {
     const [posts, setposts] = useState([]);
@@ -13,16 +14,29 @@ function Course() {
         getPosts().then(resoults => setposts(resoults));
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
- 
+
     return (
         <>
+            <Breadcrumb>
+                <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+                <Breadcrumb.Item href={`/course/${courseId}`}>
+                    Courses</Breadcrumb.Item>
+                <Breadcrumb.Item active>Leasons</Breadcrumb.Item>
+            </Breadcrumb>
             <h1>Leasons:</h1>
-            <ul>
+
+            <ListGroup>
                 {!!posts.length && posts.map(post => (
-                    <li key={post.id}>
-                        <Link to={`/leason/${courseId}/${post.id}`}>{post.title.rendered}</Link>
-                    </li>))}
-            </ul>
+                    <ListGroup.Item key={post.id}>
+                        <Link to={`/leason/${courseId}/${post.id}`}>
+                            <img
+                                alt=""
+                                src="/lesson.png"
+                                width="20"
+                                height="20" className='me-4' />
+                            {post.title.rendered}</Link>
+                    </ListGroup.Item>))}
+            </ListGroup>
         </>
 
     );
--- src/components/pages/Leason.js
+++ src/components/pages/Leason.js
@@ -1,25 +1,32 @@
-import React from 'react'; 
+import React from 'react';
 import { useParams } from "react-router-dom";
 import getPost from '../../services/getPost';
-  
+import { Breadcrumb } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 
 function Leason() {
-    const [leason, setLeason] = useState([]); 
+    const [leason, setLeason] = useState([]);
     let { courseId, leasonId } = useParams();
- 
- 
+
+
     useEffect(() => {
         !!leasonId && getPost(leasonId).then(post => setLeason(post));
     }, [leasonId]);
- 
+
     return (
         <>
-       
-             {!!courseId && !!leasonId && !!leason?.content?.rendered && (
+            <Breadcrumb>
+                <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+                <Breadcrumb.Item href={`/course/${courseId}`}>
+                    Leasons
+                </Breadcrumb.Item>
+                <Breadcrumb.Item active>Leason</Breadcrumb.Item>
+            </Breadcrumb>
+            {!!courseId && !!leasonId && !!leason?.content?.rendered && (
                 <>
                     <h1 className='pb-4'>{leason.title.rendered}</h1>
                     <div dangerouslySetInnerHTML={{ __html: leason.content.rendered }} />
+
                 </>
             )}
 
