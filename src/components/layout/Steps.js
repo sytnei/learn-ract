44f8adcafe4c2958dbca5ca6fa8560999553d374
sytnei <ionut_dina@yahoo.com>
2022-11-20 14:06:39 +0200

    Update steps

--- src/components/layout/Steps.js
+++ src/components/layout/Steps.js
@@ -3,36 +3,28 @@ import { useParams } from "react-router-dom";
 import getPosts from '../../services/getPosts';
 import { useState, useEffect } from 'react';
 
-function Steps () {
-    const [posts, setposts] = useState([]);
-    let { courseId , leasonId} = useParams();
+function Steps() {
+  const [posts, setposts] = useState([]);
+  let { courseId, leasonId } = useParams();
 
-    useEffect(() => {
-        getPosts().then(resoults => setposts(resoults));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  useEffect(() => {
+    getPosts().then(resoults => setposts(resoults));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return ([
-        <div class="stepper-wrapper">
-  
-        {!!posts.length && posts.map((post, index) => (
+  return (
+    <div class="stepper-wrapper">
 
-            <div className={`stepper-item ${leasonId < post.id ? 'completed':''} ${parseInt(leasonId) === post.id ? 'active':''}`}>
-          <div className="step-counter">{index+1}</div>
+      {!!posts.length && posts.map((post, index) => (
+
+        <div className={`stepper-item ${leasonId < post.id ? 'completed' : ''} ${parseInt(leasonId) === post.id ? 'active' : ''}`}>
+          <div className="step-counter">{index + 1}</div>
           <div className="step-name">{post.title.rendered}</div>
         </div>
-        ))}
-     
-      </div>]
-      
-               
-          
-          
+      ))}
 
-    )}
-
-  
-
-
-
+    </div>
+  )
+}
+ 
 export default Steps;
